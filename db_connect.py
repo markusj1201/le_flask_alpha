7221bb618d21987c6efb578f23443bfcb7adf5b6
Mark Nations <45960551+markusj1201@users.noreply.github.com>
2020-01-28 10:06:14 -0600

    Create db_connect.py

---
+++ db_connect.py
@@ -0,0 +1,4 @@
+import pyodbc
+
+pyodbc.connect(Driver={ODBC Driver 13 for SQL Server};Server=tcp:sohadev.database.windows.net,1433;Database=LE Utility;Uid=soha1;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+
