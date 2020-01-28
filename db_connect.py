b836314caf2401b0c9b83ebbd4639a40f25e2599
Mark Nations <45960551+markusj1201@users.noreply.github.com>
2020-01-28 10:13:46 -0600

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -1,4 +1,10 @@
-import pyodbc
-
-pyodbc.connect(Driver={ODBC Driver 13 for SQL Server};Server=tcp:sohadev.database.windows.net,1433;Database=LE Utility;Uid=soha1;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
-
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'tcp:myserver.database.windows.net' 
+database = 'mydb' 
+username = 'myusername' 
+password = 'mypassword' 
+cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:sohadev.database.windows.net,1433;Database=LE Utility;Uid=soha1;Pwd={QzPmTv713?};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;)
+cursor = cnxn.cursor()
